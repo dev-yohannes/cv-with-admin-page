9329206b89e8d207429fa6dcb944a460864ca996
Yohannes Mulu <dev.yohannes@gmail.com>
2023-10-30 15:18:22 +0300

    working on import and export

--- admin page/admin.js
+++ admin page/admin.js
@@ -1,1 +1,6 @@
-// export const table = document.querySelector("table");
+export const table = document.querySelector("table");
+// export default { table };
+
+export const fname = "jo";
+
+console.log(fname);

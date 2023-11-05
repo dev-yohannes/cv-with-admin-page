54a75794edfe1f07614e8be0aec12b8dc1206a7d
Yohanens Mulu <dev.yohannes@gmail.com>
2023-11-05 17:25:13 +0300

    moving it to root folder

---
+++ script.js
@@ -0,0 +1,85 @@
+const fullNameInput = document.querySelector("#full-name-input");
+const proTitleInput = document.querySelector("#pro-title-input");
+const countryInput = document.querySelector("#country-input");
+const cityInput = document.querySelector("#city-input");
+const phoneNumberInput = document.querySelector("#po-number-input");
+const emailInput = document.querySelector("#email-input");
+const websiteLinks = document.querySelector("#website-link-input");
+const submitButton = document.querySelector("#submit");
+const successErrorMessage = document.querySelector("#success-error-text");
+
+successErrorMessage.style.display = "none";
+
+const usersDataArray = [];
+
+submitButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (
+    fullNameInput.value === "" ||
+    proTitleInput.value === "" ||
+    countryInput.value === "" ||
+    cityInput.value === "" ||
+    phoneNumberInput.value === "" ||
+    emailInput.value === "" ||
+    websiteLinks.value === ""
+  ) {
+    successErrorMessage.style.display = "block";
+    successErrorMessage.textContent = "Error! check again";
+    successErrorMessage.style.backgroundColor = "red";
+    successErrorMessage.style.color = "white";
+
+    setInterval(() => {
+      successErrorMessage.style.display = "none";
+    }, 1000);
+  } else {
+    successErrorMessage.style.display = "block";
+    successErrorMessage.textContent = "Submitted";
+    successErrorMessage.style.backgroundColor = "lightgreen";
+
+    localStorage.setItem("userFullName", fullNameInput.value);
+    const userFullName = localStorage.getItem("userFullName");
+    localStorage.setItem("userProfessionalTitle", proTitleInput.value);
+    const userProfessionalTitle = localStorage.getItem("userProfessionalTitle");
+    localStorage.setItem("userCountry", countryInput.value);
+    const userCountry = localStorage.getItem("userCountry");
+    localStorage.setItem("userCity", cityInput.value);
+    const userCity = localStorage.getItem("userCity");
+    localStorage.setItem("userPhoneNumber", phoneNumberInput.value);
+    const userPhoneNumber = localStorage.getItem("userPhoneNumber");
+    localStorage.setItem("userEmail", emailInput.value);
+    const userEmail = localStorage.getItem("userEmail");
+    localStorage.setItem("userWebsite", websiteLinks.value);
+    const userWebsite = localStorage.getItem("userWebsite");
+
+    const usersDataObject = {};
+
+    usersDataObject.userFullName = userFullName;
+    usersDataObject.userProfessionalTitle = userProfessionalTitle;
+    usersDataObject.userCountry = userCountry;
+    usersDataObject.userCity = userCity;
+    usersDataObject.userPhoneNumber = userPhoneNumber;
+    usersDataObject.userEmail = userEmail;
+    usersDataObject.userWebsite = userWebsite;
+
+    usersDataArray.push(usersDataObject);
+    console.log(usersDataArray);
+
+    const usersDataArrayJson = JSON.stringify(usersDataArray);
+    // console.log(usersDataArrayJson);
+
+    localStorage.setItem("stringifyUsersDataArray", usersDataArrayJson);
+
+    fullNameInput.value = "";
+    proTitleInput.value = "";
+    countryInput.value = "";
+    cityInput.value = "";
+    phoneNumberInput.value = "";
+    emailInput.value = "";
+    websiteLinks.value = "";
+
+    setInterval(() => {
+      successErrorMessage.style.display = "none";
+    }, 2000);
+  }
+});

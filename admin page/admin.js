7e6639e3e7072f3300e4cce71f275e30c1091f93
Yohanens Mulu <dev.yohannes@gmail.com>
2023-11-05 17:27:06 +0300

    looping through the array and displaying the data on their own table row

--- admin page/admin.js
+++ admin page/admin.js
@@ -1,8 +1,29 @@
-// import { usersDataArrayLocalStorage } from "../input page/script.js";
-// console.log(usersDataArrayLocalStorage);
+const tabel = document.querySelector("table");
 
-// import { usersDataArrayJson } from "../input page/script.js";
-// const usersDataArray = JSON.parse(usersDataArrayJson);
-// console.log(usersDataArray);
-const saving = localStorage.getItem("storedUserDataArray");
-console.log(saving);
+const parseUsersDataArray = JSON.parse(
+  localStorage.getItem("stringifyUsersDataArray")
+);
+console.log("saved data", parseUsersDataArray);
+
+parseUsersDataArray.forEach((object, i) => {
+  console.log(object);
+
+  const userFullName = parseUsersDataArray[i].userFullName;
+  const userProfessionalTitle = parseUsersDataArray[i].userProfessionalTitle;
+  const userCountry = parseUsersDataArray[i].userCountry;
+  const userCity = parseUsersDataArray[i].userCity;
+  const userPhoneNumber = parseUsersDataArray[i].userPhoneNumber;
+  const userEmail = parseUsersDataArray[i].userEmail;
+  const userWebsite = parseUsersDataArray[i].userWebsite;
+
+  const newTr = document.createElement("tr");
+  newTr.innerHTML = `
+    <td>${userFullName}</td>
+    <td>${userProfessionalTitle}</td>
+    <td>${userCountry}/ ${userCity}</td>
+    <td>${userPhoneNumber}</td>
+    <td>${userEmail}</td>
+    <td>${userWebsite}</td>
+`;
+  tabel.appendChild(newTr);
+});
